fc3ede8ec55875e61557831139c5a98d62d39e0b
adindasintawati <adindasintawati@gmail.com>
2024-10-03 12:55:39 +0700

    project nexjs

--- src/components/atoms/Paragraph/index.js
+++ src/components/atoms/Paragraph/index.js
@@ -6,9 +6,9 @@ const Paragraph = ({ className = "" }) => {
       className={`text-[20px] leading-[33px] relative w-3/4 float-left pr-[200px] ${className}`}
     >
       <p>
-        I'm a design leader with a passion for simple and elegant solutions. I
+        Im a design leader with a passion for simple and elegant solutions. I
         enjoy close collaboration, and support designers to create excellent
-        work that's aligned to user needs and business goals.
+        work thats aligned to user needs and business goals.
       </p>
     </div>
   );
